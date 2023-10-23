04c0f89c6975e4ef7213b7eb14857e3c40d5379d
karriv18 <krlclarence@gmail.com>
2023-10-23 18:45:53 +0800

    dynamic

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ class Layer{
         this.y = 0; 
         this.width = 2400; 
         this.height = 700; 
-        this.x2 = this.width;
+        // this.x2 = this.width;
         this.image = image; 
         this.speedModifier = speedModifier;
         this.speed = gameSpeed * this.speedModifier;
@@ -41,18 +41,18 @@ class Layer{
     update(){
         this.speed = gameSpeed * this.speedModifier; 
         if (this.x <= -this.width){
-            this.x = this.width + this.x2 - this.speed;
+            this.x = 0;
         }
-        if (this.x2 <= this.width){
-            this.x2 = this.width + this.x - this.speed;
-        }
+        // if (this.x2 <= this.width){
+        //     this.x2 = this.width + this.x - this.speed;
+        // }
         this.x = Math.floor(this.x - this.speed)
-        this.x2 = Math.floor(this.x2 - this.speed)
+        // this.x2 = Math.floor(this.x2 - this.speed)
     }
     draw(){
         // drawing in canvas x axis
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-        ctx.drawImage(this.image, this.x2, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
     }
 }
 const layer1 = new Layer(backgroundLayer1, 0.2)
