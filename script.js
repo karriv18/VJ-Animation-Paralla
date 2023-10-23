5406bb82bf85728c85fcea52cf98f9844834f457
karriv18 <krlclarence@gmail.com>
2023-10-23 16:39:23 +0800

    push

--- script.js
+++ script.js
@@ -20,12 +20,11 @@ let x2 = 2400;
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
     ctx.drawImage(backgroundLayer4, x, 0)
-    if (x < -2400){
-        x = 2400;
-    }
-    else{
-    x -= gameSpeed;
-    }
+    ctx.drawImage(backgroundLayer4, x2, 0)
+    if (x < -2400) x = 2400 - gameSpeed;
+    else x -= gameSpeed;
+    if (x2 < -2400) x2 = 2400 - gameSpeed;
+    else x2 -= gameSpeed;
     requestAnimationFrame(animate)
 }
 
