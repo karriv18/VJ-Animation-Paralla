bf8ae6bf716548c947783a676ee6190dd853cd05
karriv18 <krlclarence@gmail.com>
2023-10-23 18:38:56 +0800

    adding range

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d')
 const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.height = 700;
-let gameSpeed = 5;
+let gameSpeed = 0;
 
 const backgroundLayer1 = new Image(); 
 backgroundLayer1.src = 'images/layer-1.png'
@@ -15,6 +15,17 @@ backgroundLayer4.src = 'images/layer-4.png'
 const backgroundLayer5 = new Image()
 backgroundLayer5.src = 'images/layer-5.png'
 
+const slider = document.getElementById('slider'); 
+slider.value = gameSpeed; 
+const showGameSpeed = document.getElementById('showGameSpeed') 
+showGameSpeed.innerHTML = gameSpeed; 
+
+slider.addEventListener('change', (e) => {
+    let speed = e.target.value
+    gameSpeed = speed
+    showGameSpeed.innerHTML = gameSpeed; 
+})
+// blueprint for sprite animation 
 class Layer{ 
     constructor(image, speedModifier){
         // "this" means =  the current object within the scope
@@ -29,7 +40,7 @@ class Layer{
     }
     update(){
         this.speed = gameSpeed * this.speedModifier; 
-        if (this.x <= this.width){
+        if (this.x <= -this.width){
             this.x = this.width + this.x2 - this.speed;
         }
         if (this.x2 <= this.width){
@@ -39,14 +50,25 @@ class Layer{
         this.x2 = Math.floor(this.x2 - this.speed)
     }
     draw(){
+        // drawing in canvas x axis
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
         ctx.drawImage(this.image, this.x2, this.y, this.width, this.height);
     }
-    
 }
+const layer1 = new Layer(backgroundLayer1, 0.2)
+const layer2 = new Layer(backgroundLayer2, 0.4)
+const layer3 = new Layer(backgroundLayer3, 0.6)
+const layer4 = new Layer(backgroundLayer4, 0.8)
+const layer5 = new Layer(backgroundLayer5, 1)
+
+const gameObjects = [layer1, layer2, layer3, layer4, layer5];
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+    gameObjects.forEach((state) => {
+        state.update(); 
+        state.draw();
+    })
     requestAnimationFrame(animate)
 }
  
